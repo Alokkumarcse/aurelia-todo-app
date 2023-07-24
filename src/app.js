e9b9d9383b1e9856b47c418fc06b33caa4208cff
Alok Kumar <alokk13799@gmail.com>
2023-07-24 19:08:39 +0530

    basic router added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
+import { PLATFORM } from "aurelia-framework";
 export class App {
   constructor() {
     this.message = "Hello World!";
+  }
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = "Router Test";
+    config.map([
+      {
+        route: "",
+        name: "home",
+        moduleId: PLATFORM.moduleName("./page/home/index"),
+        title: "Home",
+      },
+      {
+        route: "todo",
+        name: "task",
+        moduleId: PLATFORM.moduleName("./page/task/todo"),
+        title: "Task",
+      },
+    ]);
   }
 }
